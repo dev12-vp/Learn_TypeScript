425ef5618fe77038c950e7db6d615750e887feec
Vivek <vivek@medianv.com>
2025-08-06 17:34:25 +0530

    add simpl type and special type

--- type/type.ts
+++ type/type.ts
@@ -1,5 +1,35 @@
 // type is more versatile, allowing unions, intersections, and more complex type definitions.
 
+//simpl type
+
+let myName: string = "vivel" ;
+console.log(myName);
+
+let myAge : number = 25;
+console.log(myAge);
+
+let bool : boolean = true ;
+console.log(bool)
+
+let bigint : bigint = BigInt(123212);
+console.log(bigint);
+
+let bigint1 : number = Number(BigInt(12321232));
+console.log(bigint1);
+
+
+// special type
+
+let yourNumber : any = "ten";
+console.log("yourNumbery",yourNumber);
+yourNumber = 10;
+console.log("yourNumbery1",yourNumber);
+
+
+let val : unknown = "test";
+val = 10 
+console.log('val', val);
+
 type userValue = {
     name : string,
     age : number 
